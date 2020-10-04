65d484648d8a6e69a9f9bf3cdce6f13a62a3203b
toushi100 <toushi2009@gmail.com>
2020-10-04 12:31:06 +0200

    first commit and chapter one

---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2
+#Importing and displaying an image
+print("Package Imported")
+img = cv2.imread("image.jpg")
+img = cv2.sca
+cv2.imshow("Output Window", img)
+cv2.waitKey(0)
+
+#Importing and playing a video
+
+cap = cv2.VideoCapture("video.mp4")
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video window",img)
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
+        break
+
+#Using Webcam for video
+
+cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
+
+#setting the Exposure for webcam
+cap.set(10,100)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video window",img)
+    if cv2.waitKey(1) & 0xFF ==ord('q'):
+        break
