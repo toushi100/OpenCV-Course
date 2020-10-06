31f9a3d3d9e4d54314f920241fa074c45b25214f
toushi100 <toushi2009@gmail.com>
2020-10-06 16:40:03 +0200

    Chapter nine

--- Chapter nine.py
+++ Chapter nine.py
@@ -6,11 +6,11 @@ faceCascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
 img = cv.imread("image.jpg")
 img = cv.resize(img,(400,600))
 imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-cv.imshow("Vertical Image", imgGray)
+
 
 faces = faceCascade.detectMultiScale(imgGray,1.1,4)
 for (x,y,w,h) in faces:
     cv.rectangle(img,(x,y),(x+w,y+h),(0,0,0),4)
 
-
+cv.imshow("Result Image", img)
 cv.waitKey(0)
